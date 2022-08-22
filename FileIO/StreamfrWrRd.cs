63a03128ea2a568c54a07454d750feea1e212484
rajashrip096 <rajashrip096@gmail.com>
2022-08-22 18:10:48 +0530

    Upload 15

--- FileIO/StreamfrWrRd.cs
+++ FileIO/StreamfrWrRd.cs
@@ -7,30 +7,29 @@ using System.IO;
 
 namespace MyNewProject1.FileIO
 {
-    //Write a code to accept string from user & write to file using Stream writer & read & display on console
-   class AssignmtSteamWrRd
+    class StreamfrWrRd
     {
-        static void WriteToFilestream()
+        static void WriteFilestream()
         {
             try
             {
-                FileStream fs = new FileStream(@"E:\Dotnetcore\TestFolder\TextDoc.txt", FileMode.Create,FileAccess.Write);
+                FileStream fs = new FileStream(@"E:\Dotnetcore\TestFolder\TextDoc1.txt", FileMode.Create, FileAccess.Write);
                 StreamWriter sw = new StreamWriter(fs);
-                sw.WriteLine("This is Sample text in the File");
+                sw.WriteLine("This is Sample text ");
                 sw.Close();
                 sw.Close();
                 Console.WriteLine("Done");
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
         }
-        static void ReadFromUsingstream()
+        static void ReadFrUsingstream()
         {
             try
-            { 
-                FileStream fs = new FileStream(@"E:\Dotnetcore\TestFolder\TextDoc.txt", FileMode.Open, FileAccess.Read);
+            {
+                FileStream fs = new FileStream(@"E:\Dotnetcore\TestFolder\TextDoc1.txt", FileMode.Open, FileAccess.Read);
                 StreamReader sr = new StreamReader(fs);
                 Console.WriteLine(sr.ReadToEnd());
                 sr.Close();
@@ -43,8 +42,8 @@ namespace MyNewProject1.FileIO
         }
         static void Main(string[] args)
         {
-            WriteToFilestream();
-            ReadFromUsingstream();
+            WriteFilestream();
+            ReadFrUsingstream();
             Console.ReadLine();
         }
     }

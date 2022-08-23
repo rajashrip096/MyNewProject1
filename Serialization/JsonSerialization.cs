8af65fdb4fa50e368502724f39a83a27496100d2
rajashrip096 <rajashrip096@gmail.com>
2022-08-23 11:55:11 +0530

    upload15

---
+++ Serialization/JsonSerialization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace MyNewProject1.Serialization
+{
+    class JsonSerialization
+    {
+    }
+}
---
+++ Serialization/SoapSerialization.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace MyNewProject1.Serialization
+{
+    [Serializable]
+    public class Student2
+    {
+        int rollno;
+        string name;
+        double percentage;
+
+        public int Rollno { get => rollno; set => rollno = value; }
+        public string Name { get => name; set => name = value; }
+        public double Percentage { get => percentage; set => percentage = value; }
+    }
+    class SoapSerialization
+    {
+        static void SoapSerializationWrite(Student2 stud)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"E:\Dotnetcore\FileFolder\SoapFile.soap", FileMode.Create, FileAccess.Write);
+                SoapFormatter sf = new SoapFormatter();
+                sf.Serialize(fs, stud);
+                Console.WriteLine("Soap data added");
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void SoapSerializationRead()
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"E:\Dotnetcore\FileFolder\SoapFile.soap", FileMode.Open, FileAccess.Read);
+                SoapFormatter sf = new SoapFormatter();
+                Student2 stud = (Student2)sf.Deserialize(fs);
+                Console.WriteLine(stud.Rollno);
+                Console.WriteLine(stud.Name);
+                Console.WriteLine(stud.Percentage);
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student2 stud = new Student2 { Rollno = 100, Name = "Rajesh", Percentage = 88.44 };
+            SoapSerializationWrite(stud);
+            SoapSerializationRead();
+            Console.ReadLine();
+        }
+    }
+}

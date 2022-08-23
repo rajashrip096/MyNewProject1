4ee481947af9617eb6c2171e77c92958150dc1eb
rajashrip096 <rajashrip096@gmail.com>
2022-08-23 11:25:57 +0530

    upload15

---
+++ Serialization/XmlSerialization.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace MyNewProject1.Serialization
+{
+
+    [Serializable]
+    public class Student1
+    {
+        int rollno;
+        string name;
+        double percentage;
+
+        public int Rollno { get => rollno; set => rollno = value; }
+        public string Name { get => name; set => name = value; }
+        public double Percentage { get => percentage; set => percentage = value; }
+    }
+    class XmlSerialization
+    {
+        static void XmlSerializationWrite(Student1 stud)
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"E:\Dotnetcore\FileFolder\XmlFile.xml", FileMode.Create, FileAccess.Write);
+                XmlSerializer xs = new XmlSerializer(typeof(Student1));
+                xs.Serialize(fs, stud);
+                Console.WriteLine("Xml data added");
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+         static void XmlSerializationRead()
+        {
+            try
+            {
+                FileStream fs = new FileStream(@"E:\Dotnetcore\FileFolder\XmlFile.xml", FileMode.Open, FileAccess.Read);
+                XmlSerializer xs = new XmlSerializer(typeof(Student1));
+                Student1 stud = (Student1)xs.Deserialize(fs);
+                Console.WriteLine(stud.Rollno);
+                Console.WriteLine(stud.Name);
+                Console.WriteLine(stud.Percentage);
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student1 stud = new Student1 { Rollno = 100, Name = "Rajesh", Percentage = 88.44 };
+            XmlSerializationWrite(stud);
+            XmlSerializationRead();
+            Console.ReadLine();
+        }
+    }
+}
